ee5163528cbe7426619a1206b3164be677adde7b
RoughGhost <Stephendlouis6@gmail.com>
2021-11-18 00:41:07 +0100

    main food

---
+++ src/components/Footer/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FaInstagram, FaFacebook, 
+FaYoutube, FaTwitter, } from 'react-icons/fa'; 
+import {FooterContainer, FooterWrap, 
+SocialMedia, SocialMediaWrap, SocialLogo, SocialIcons, 
+SocialIconLink} from './FooterElements'
+
+ const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                    <SocialLogo to="/">Pizza</SocialLogo>
+                     <SocialIcons>
+                         <SocialIconLink href="/" target="_blank"
+                         aria-label="Facebook" rel="noopener noreferrer">
+                            <FaFacebook/>
+                         </SocialIconLink>
+                     </SocialIcons>
+                     <SocialIcons>
+                         <SocialIconLink href="/" target="_blank"
+                         aria-label="Instagram" rel="noopener noreferrer">
+                            <FaInstagram/>
+                         </SocialIconLink>
+                     </SocialIcons>
+                     <SocialIcons>
+                         <SocialIconLink href="/" target="_blank"
+                         aria-label="Twitter" rel="noopener noreferrer">
+                            <FaTwitter/>
+                         </SocialIconLink>
+                     </SocialIcons>
+                     <SocialIcons>
+                         <SocialIconLink href="/" target="_blank"
+                         aria-label="Youtube" rel="noopener noreferrer"s>
+                            <FaYoutube/>
+                         </SocialIconLink>
+                     </SocialIcons>
+                        </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+            </FooterContainer>
+
+    )
+}
+
+export default Footer;
---
+++ src/components/Products/data.js
@@ -0,0 +1,74 @@
+import product1 from '../../images/nicolas-perondi-UxRhrU8fPHQ-unsplash.jpg';
+import product2 from '../../images/saahil-khatkhate-kfDsMDyX1K0-unsplash.jpg';
+import product3 from '../../images/sahand-hoseini-BMVAYjPf6mU-unsplash.jpg';
+import sweet1 from '../../images/istockphoto-669973646-170667a.jpg';
+import sweet2 from '../../images/vitor-monthay.jpg';
+import sweet3 from '../../images/adam-bartoszewicz.jpg';
+
+
+
+
+export const productData = [
+ {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, tomatoes, olives and pesto', 
+    price: '$12.50',
+    button: 'Add to Cart'
+},
+ {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, tomatoes, olives and pesto', 
+    price: '$20.00',
+    button: 'Add to Cart'
+},
+ {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, tomatoes, olives and pesto', 
+    price: '$35.00',
+    button: 'Add to Cart'
+},
+
+
+
+
+];
+
+
+
+
+export const productDataTwo = [
+ {
+    img: sweet1,
+    alt: 'Juice',
+    name: 'Fruit Juice',
+    desc: 'Orange, Grape, Tomatoes ', 
+    price: '$7.00',
+    button: 'Add to Cart'
+},
+ {
+    img: sweet2,
+    alt: 'Cake',
+    name: 'Chocolate Cup Cake',
+    desc: 'Choclate, Strawbrries, Vanilla Essence', 
+    price: '$10.00',
+    button: 'Add to Cart'
+},
+ {
+    img: sweet3,
+    alt: 'Salad',
+    name: 'Salad ',
+    desc: 'Cabbage, Lettuce, Cucumber, Green Pepper, Carrot', 
+    price: '$5.00',
+    button: 'Add to Cart'
+},
+
+
+
+
+];
